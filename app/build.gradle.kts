462acfc851d5e18e6fe18d5684f7ba4a2922b79c
steven750MC <Yasinmazahery@yahoo.com>
2026-09-02 18:18:37 +0330

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,4 +75,17 @@ dependencies {
     debugImplementation(libs.jetpackCompose.uiTest.manifest)
     androidTestImplementation(libs.jetpackCompose.uiTest.junit)
     androidTestImplementation(libs.test.androidCore)
+
+implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
+    // ViewModel در Compose
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.4")
+    implementation("androidx.core:core-ktx:1.13.1")
+
+    // کوروتین‌ها (اگر از قبل نیست)
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
+
 }
